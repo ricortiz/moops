56029e32394044e84ebebfd1ef48db8bb7e9e09c
Ricardo Ortiz <ortiz@unc.edu>
2012-04-12 23:28:21 -0400

    adding ways to split the rhs operator to perform semi-implicit timestepping in GPU.

--- math/fluid_solver/stokes/gpu_stokes_solver.hpp
+++ math/fluid_solver/stokes/gpu_stokes_solver.hpp
@@ -27,7 +27,12 @@ class GpuStokesSolver
         value_type m_delta;
         size_t m_num_sources;
         bool m_images;
+        std::vector<value_type> buffer[2];
         
+    public:
+        idx_vector      col_ptr;
+        idx_vector      col_idx;
+
     public:
         GpuStokesSolver(size_t num_sources) : m_num_sources(num_sources), m_images(false) {}
         
@@ -42,6 +47,60 @@ class GpuStokesSolver
             ComputeStokeslets(x,v,y,f,m_delta,m_num_sources,num_targets,m_images);
         }
 
+        inline void Implicit ( value_type, const value_type *x, value_type *v, const value_type *y, const value_type *f )
+        {
+            std::fill(v,v+m_num_sources*3,0.0);
+            for ( size_t p = 0; p < col_ptr.size()-1; ++p )
+                for(size_t j = col_ptr[p], end = col_ptr[p+1]; j != end; ++j)
+                    ;
+            
+        }
+
+        inline void Explicit ( value_type, const value_type *x, value_type *v, const value_type *y, const value_type *f )
+        {
+            std::fill(v,v+m_num_sources*3,0.0);
+            std::vector<size_t>::iterator begin,end;
+            for ( size_t p = 0; p < col_ptr.size()-1; ++p)
+            {
+                size_t sidx = 3*p;
+                begin = col_idx.begin()+col_ptr[p];
+                end = col_idx.begin()+col_ptr[p+1];
+                // NOTE: col_idx sould have sorted column indices
+                size_t min = col_idx[col_ptr[p]];
+                size_t max = col_idx[col_ptr[p+1]-1];
+                
+                for(size_t j = 0; j < min; ++j)
+                {
+                    if (j == p) continue;
+                    size_t tidx = 3*j;
+                    computeStokeslet ( &x[tidx], &v[tidx], &y[sidx], &f[sidx], m_delta );
+                }
+                
+                std::vector<size_t> tmp(max-min+1), idx(max-min+1-(col_ptr[p+1]-col_ptr[p]));
+                generateSequence(tmp,min);
+                std::set_difference(tmp.begin(),tmp.end(),begin,end,idx.begin());
+                for(size_t j = 0; j < idx.size(); ++j)
+                {
+                    if (idx[j] == p) continue;
+                    size_t tidx = 3*idx[j];
+                    computeStokeslet ( &x[tidx], &v[tidx], &y[sidx], &f[sidx], m_delta );
+                }
+                
+                for(size_t j = max+1; j < m_num_sources; ++j)
+                {
+                    if (j == p) continue;
+                    size_t tidx = 3*j;
+                    computeStokeslet ( &x[tidx], &v[tidx], &y[sidx], &f[sidx], m_delta );
+                }
+            }
+        }
+        
+        void generateSequence(std::vector<size_t> &seq, size_t start)
+        {
+            for(size_t k = 0; k != seq.size(); ++k)
+                seq[k] = start++;
+        }
+        
         void setDelta(value_type delta) { m_delta = delta; }
         void withImages(bool images) { m_images = images; }
         
